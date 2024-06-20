38c15fc88920512e6cb3c17de6d35ed6db598bfa
nikhilsingh1999 <brajendrarajpoot80@gmail.com>
2024-06-20 19:26:03 +0530

    First commit

---
+++ sign_up_js.js
@@ -0,0 +1,22 @@
+function open_menu() {
+    document.getElementById('sidemenubar').style.left = '0%';
+}
+
+function close_menu() {
+    document.getElementById('sidemenubar').style.left = "-100%";
+}
+
+
+
+
+let fname=document.getElementById('First-name').value;
+
+let lname=document.getElementById('Last-name').value;
+
+let fphone=document.getElementById('Phone').value;
+
+let femail=document.getElementById('E-mail').value;
+
+let fpass=document.getElementById('re-password').value;
+
+let frepass=document.getElementById('re-password').value;
